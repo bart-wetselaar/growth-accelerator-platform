03044e61ec3d2a7b89bcac8d28176589038c43cc
bart-wetselaar <bart@growthaccelerator.nl>
2025-06-01 19:46:23 +0200

    Deploy complete Growth Accelerator platform with authentic Workable API integration

--- main.py
+++ main.py
@@ -7,5 +7,5 @@ from staffing_app import app
 
 if __name__ == '__main__':
     import os
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port, debug=True)
+    port = int(os.environ.get('PORT', 8000))
+    app.run(host='0.0.0.0', port=port, debug=False)
