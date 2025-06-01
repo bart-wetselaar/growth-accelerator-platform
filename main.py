54f885281647f87add25c80c0c7dc6ead8789648
bart-wetselaar <bart@growthaccelerator.nl>
2025-06-01 16:44:46 +0200

    Add main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+"""
+Main entry point for Growth Accelerator Staffing Platform on Azure
+"""
+
+# Import the complete staffing application
+from staffing_app import app
+
+if __name__ == '__main__':
+    import os
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
